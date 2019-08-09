d306547a5579ab5a3bd5c501777b82c3a96c5782
Artuk Kakhorov <kakhoatu@schaeffler.com>
2019-08-09 15:23:52 +0200

    Update NewBehaviourScript.cs

--- CreatorGithubProject/Assets/Scenes/NewBehaviourScript.cs
+++ CreatorGithubProject/Assets/Scenes/NewBehaviourScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[HelpURL("https://github.com/artukbay/CreatorRepo/blob/master/Documentation/testDocu.html")]
+[HelpURL("https://github.com/artukbay/CreatorRepo/blob/master/Documentation/index.html")]
 public class NewBehaviourScript : MonoBehaviour
 {
 
