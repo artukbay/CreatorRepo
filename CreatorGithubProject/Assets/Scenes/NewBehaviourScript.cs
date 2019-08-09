37d30b2f7688db800a40c8fbc35d1f9c71ecf685
Artuk Kakhorov <kakhoatu@schaeffler.com>
2019-08-09 15:22:09 +0200

    asd

---
+++ CreatorGithubProject/Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[HelpURL("https://github.com/artukbay/CreatorRepo/blob/master/Documentation/testDocu.html")]
+public class NewBehaviourScript : MonoBehaviour
+{
+
+
+}
